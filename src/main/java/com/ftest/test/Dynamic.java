66f3cfacdcb0da0a0e22c2f1bf131c9b2152c7e9
wty4427300 <wty4427300@gmail.com>
2020-07-27 15:45:47 +0800

    动规买股票

---
+++ src/main/java/com/ftest/test/Dynamic.java
@@ -0,0 +1,25 @@
+package com.ftest.test;
+
+/**
+ * 动规买股票
+ */
+public class Dynamic {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        // 第一天不卖出，只买入，所以卖出收益为0
+        dp[0][0] = 0;
+        //第一天的买入收益就是买入价格本身
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            //第i天卖出的最大收益
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            //第i天买入的最大收益
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+}
