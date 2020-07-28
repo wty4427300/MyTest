f54df18f63842f365c30374734d051ed0002e9d4
wty4427300 <wty4427300@gmail.com>
2020-07-28 18:02:34 +0800

    最大子序和的dp解法

--- src/main/java/com/ftest/test/Dynamic.java
+++ src/main/java/com/ftest/test/Dynamic.java
@@ -39,4 +39,28 @@ public class Dynamic {
         }
         return dp[n];
     }
+
+    /**
+     * @param nums
+     * @return
+     * 最大子数组和
+     */
+    public int maxSubArray(int[] nums) {
+        if (nums.length<1){
+            return 0;
+        }
+        int[] dp=new int[nums.length];
+        int result=nums[0];
+        dp[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if (dp[i-1]<0){
+                dp[i]=nums[i];
+            }else {
+                dp[i]=dp[i-1]+nums[i];
+            }
+            //result不一定是最后的值，也有可能是中间结果
+            result=Math.max(dp[i],result);
+        }
+        return result;
+    }
 }
