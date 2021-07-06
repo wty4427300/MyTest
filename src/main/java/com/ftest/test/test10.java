5703973010daca1d9cdd3b9b49db41debbaca0e4
wty4427300 <wty4427300@gmail.com>
2021-07-06 14:09:46 +0800

    垃圾代码

---
+++ src/main/java/com/ftest/test/test10.java
@@ -0,0 +1,28 @@
+package com.ftest.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class test10 {
+    public static void main(String[] args) {
+//        List<Integer> list = new ArrayList<>();
+//        for (int i = 0; i < 100000000; i++) {
+//            list.add(i);
+//        }
+//        System.out.println(list.size());
+//
+//        List<Integer> list1 = new ArrayList<>();
+//        list.stream().forEach(x -> list1.add(x));
+//        System.out.println(list1.size());
+
+//        List<Integer> list2 = new ArrayList<>();
+//        list.parallelStream().forEach(x -> list2.add(x));
+//        System.out.println(list2.size());
+
+//        List<Integer> list3 = list.parallelStream().collect(Collectors.toList());
+//        System.out.println(list3.size());
+        String a="[{\"itemId\":34,\"price\":360.0,\"type\":2},{\"itemId\":35,\"price\":269.0,\"type\":2},{\"itemId\":36,\"price\":399.0,\"type\":2},{\"itemId\":37,\"price\":549.0,\"type\":2},{\"itemId\":38,\"price\":399.0,\"type\":2},{\"itemId\":39,\"price\":549.0,\"type\":2},{\"itemId\":41,\"price\":99.0,\"type\":2},{\"itemId\":46,\"price\":49.0,\"type\":2},{\"itemId\":47,\"price\":1299.0,\"type\":2},{\"itemId\":19,\"price\":149.0,\"type\":2},{\"itemId\":20,\"price\":229.0,\"type\":2},{\"itemId\":21,\"price\":299.0,\"type\":2},{\"itemId\":22,\"price\":1599.0,\"type\":2},{\"itemId\":25,\"price\":680.0,\"type\":2},{\"itemId\":26,\"price\":3980.0,\"type\":2},{\"itemId\":27,\"price\":1.5,\"type\":1},{\"itemId\":28,\"price\":9.9,\"type\":2},{\"itemId\":29,\"price\":9.9,\"type\":2},{\"itemId\":30,\"price\":9.9,\"type\":2},{\"itemId\":31,\"price\":9.9,\"type\":2}]";
+        System.out.println();
+    }
+}
