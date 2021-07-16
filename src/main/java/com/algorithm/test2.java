895da22c322b3a4221f445515adabf887901a001
wty4427300 <wty4427300@gmail.com>
2021-07-16 15:11:20 +0800

    太好玩了，泪目

--- src/main/java/com/algorithm/test2.java
+++ src/main/java/com/algorithm/test2.java
@@ -29,6 +29,7 @@ public class test2 {
                 StdDraw.setPenColor(StdDraw.BLUE);
             }
             StdDraw.point(i,i*i);
+            Thread.sleep(20);
             if (i%2==1){
                 StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
             }else {
@@ -37,10 +38,38 @@ public class test2 {
             StdDraw.point(i,i*Math.log(i));
         }
     }
+
+    public static void draw3() throws InterruptedException {
+        StdDraw.clear(StdDraw.BLACK);
+        StdDraw.setScale(-2, +2);
+        StdDraw.enableDoubleBuffering();
+        int i=0;
+        for (double t = 0.0; true; t += 0.01) {
+            double x = Math.sin(t);
+            double y = Math.cos(t);
+            if (i%2==0){
+                StdDraw.setPenColor(StdDraw.RED);
+            }else {
+                StdDraw.setPenColor(StdDraw.GREEN);
+            }
+            i++;
+            Thread.sleep(1);
+            StdDraw.filledCircle(x, y, 0.05);
+            if (i%2==0){
+                StdDraw.setPenColor(StdDraw.YELLOW);
+            }else {
+                StdDraw.setPenColor(StdDraw.BLACK);
+            }
+            StdDraw.filledCircle(-x, -y, 0.05);
+            StdDraw.show();
+            StdDraw.pause(20);
+        }
+    }
     public static void main(String[] args) {
         try {
-            draw1();
-            draw2();
+//            draw1();
+//            draw2();
+            draw3();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
