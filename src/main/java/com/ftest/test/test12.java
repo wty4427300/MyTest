48077f5a85af3e5091a87004f8e0ff4eb6b00fbd
wty4427300 <wty4427300@gmail.com>
2021-10-30 19:11:01 +0800

    练习代码

---
+++ src/main/java/com/ftest/test/test12.java
@@ -0,0 +1,18 @@
+package com.ftest.test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class test12 {
+    public static void main(String[] args) {
+//        List<Integer> integers = Arrays.asList(1, 2, 3, 4);
+//        List<Integer> list1 = integers.subList(0, 2);
+//        System.out.println(list1);
+//        List<Integer> list2 = integers.subList(2, integers.size());
+//        System.out.println(list2);
+        int length=1000;
+        int groupSize=200;
+        int num = ( length + groupSize - 1 )/groupSize;
+        System.out.println(num);
+    }
+}
