cde68b6a4f064256a83bf9b2f8b8558fbcec6bac
wty4427300 <wty4427300@gmail.com>
2022-07-21 09:45:10 +0800

    format

--- src/main/java/com/algorithm/test814.java
+++ src/main/java/com/algorithm/test814.java
@@ -3,18 +3,18 @@ package com.algorithm;
 public class test814 {
 
     public TreeNode pruneTree(TreeNode root) {
-        if (root==null){
+        if (root == null) {
             return null;
         }
         //先处理子节点
-        root.left=pruneTree(root.left);
-        root.right=pruneTree(root.right);
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
         //不是叶子节点
-        if (root.left!=null || root.right!=null){
+        if (root.left != null || root.right != null) {
             return root;
         }
         //是叶子节点且
-        return root.val==0?null:root;
+        return root.val == 0 ? null : root;
     }
 
 }
