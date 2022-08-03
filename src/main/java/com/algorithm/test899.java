c756ab917e714aeff9f1fc0948dbf49281053c51
wty4427300 <wty4427300@gmail.com>
2022-08-03 14:22:57 +0800

    leetcode899

---
+++ src/main/java/com/algorithm/test899.java
@@ -0,0 +1,26 @@
+package com.algorithm;
+
+import java.util.Arrays;
+
+public class test899 {
+    public String orderlyQueue(String s, int k) {
+        if (k == 1) {
+            String smallest = s;
+            StringBuilder sb = new StringBuilder(s);
+            int n = s.length();
+            for (int i = 1; i < n; i++) {
+                char c = sb.charAt(0);
+                sb.deleteCharAt(0);
+                sb.append(c);
+                if (sb.toString().compareTo(smallest) < 0) {
+                    smallest = sb.toString();
+                }
+            }
+            return smallest;
+        } else {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            return new String(arr);
+        }
+    }
+}
