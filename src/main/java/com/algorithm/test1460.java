062b04482d2170d19ece2fb464c2c06985b0ec4e
wty4427300 <wty4427300@gmail.com>
2022-08-24 09:41:40 +0800

    leetcode1460-简单题简单做

---
+++ src/main/java/com/algorithm/test1460.java
@@ -0,0 +1,11 @@
+package com.algorithm;
+
+import java.util.Arrays;
+
+public class test1460 {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        return Arrays.equals(target, arr);
+    }
+}
