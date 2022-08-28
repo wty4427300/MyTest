166191ffdc88399703e9226cf15d442fb7fbbb93
wty4427300 <wty4427300@gmail.com>
2022-08-28 15:39:38 +0800

    leetcode662

--- src/main/java/com/algorithm/test662.java
+++ src/main/java/com/algorithm/test662.java
@@ -9,21 +9,21 @@ public class test662 {
     public int ans;
 
     public int widthOfBinaryTree(TreeNode root) {
-        this.dfs(root,1,0);
+        this.dfs(root, 1, 0);
         return ans;
     }
 
-    public void dfs(TreeNode root,int u,int depth){
-        if (root==null){
+    public void dfs(TreeNode root, int u, int depth) {
+        if (root == null) {
             return;
         }
-        if (!map.containsKey(depth)){
+        if (!map.containsKey(depth)) {
             //记录每层的第一个节点序号
-            map.put(depth,u);
+            map.put(depth, u);
         }
-        ans=Math.max(ans,u-map.get(depth)+1);
-        dfs(root.left,u<<1,depth+1);
-        dfs(root.right,u<<1|1,depth+1                                              );
+        ans = Math.max(ans, u - map.get(depth) + 1);
+        dfs(root.left, u << 1, depth + 1);
+        dfs(root.right, u << 1 | 1, depth + 1);
     }
 
     public static void main(String[] args) {
