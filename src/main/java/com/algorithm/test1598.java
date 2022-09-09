275115bb97b6e3b0d3fc35b85dbe07b07fb0262b
wty4427300 <wty4427300@gmail.com>
2022-09-09 09:43:01 +0800

    leetcode1598

---
+++ src/main/java/com/algorithm/test1598.java
@@ -0,0 +1,15 @@
+package com.algorithm;
+
+public class test1598 {
+    public int minOperations(String[] logs) {
+        int depth = 0;
+        for (String log : logs) {
+            if (log.equals("../")) {
+                depth = Math.max(0, depth - 1);
+            } else if (!log.equals("./")) {
+                depth++;
+            }
+        }
+        return depth;
+    }
+}
