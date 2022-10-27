b77808a328063d9f57078e9c6c140473195e0091
wty4427300 <wty4427300@gmail.com>
2022-10-27 09:34:34 +0800

    最近工作有点忙,又失眠了,难受

---
+++ src/main/java/com/algorithm/test1822.java
@@ -0,0 +1,17 @@
+package com.algorithm;
+
+public class test1822 {
+
+    public int arraySign(int[] nums) {
+        int sign=1;
+        for(int num:nums){
+            if (num==0){
+                return 0;
+            }
+            if (num<0){
+                sign=-sign;
+            }
+        }
+        return sign;
+    }
+}
