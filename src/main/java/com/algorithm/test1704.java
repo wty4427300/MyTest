cf405bec6d283b7c494b2f56aca6ac4110b7ab7f
wty4427300 <wty4427300@gmail.com>
2022-11-11 09:51:51 +0800

    leetcode1704

---
+++ src/main/java/com/algorithm/test1704.java
@@ -0,0 +1,22 @@
+package com.algorithm;
+
+public class test1704 {
+    public boolean halvesAreAlike(String s) {
+        int half = s.length() / 2;
+        String a = s.substring(0, half);
+        String b = s.substring(half);
+        String h = "aeiouAEIOU";
+        int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (h.indexOf(a.charAt(i)) >= 0) {
+                sum1++;
+            }
+        }
+        for (int i = 0; i < b.length(); i++) {
+            if (h.indexOf(b.charAt(i)) >= 0) {
+                sum2++;
+            }
+        }
+        return sum1 == sum2;
+    }
+}
