835b6f6ecbeba589ca64647b0ccaf63a770f00cf
wty4427300 <wty4427300@gmail.com>
2022-11-24 15:40:03 +0800

    两道leetcode

--- src/main/java/com/algorithm/test795.java
+++ src/main/java/com/algorithm/test795.java
@@ -1,8 +1,5 @@
 package com.algorithm;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
 
 public class test795 {
     public int numSubarrayBoundedMax(int[] nums, int left, int right) {
