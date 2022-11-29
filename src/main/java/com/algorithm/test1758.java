767b12bdf80a1cbd0b6ad349c0b95726c3c2f953
wty4427300 <wty4427300@gmail.com>
2022-11-29 10:10:19 +0800

    leetcode1758

---
+++ src/main/java/com/algorithm/test1758.java
@@ -0,0 +1,14 @@
+package com.algorithm;
+
+public class test1758 {
+    public int minOperations(String s) {
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c != (char) ('0' + i % 2)) {
+                cnt++;
+            }
+        }
+        return Math.min(cnt, s.length() - cnt);
+    }
+}
