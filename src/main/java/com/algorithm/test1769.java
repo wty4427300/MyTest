c6a77fe263183cbc461b4aabce9b7df54ca749be
wty4427300 <wty4427300@gmail.com>
2022-12-02 16:31:30 +0800

    leetcode1769

---
+++ src/main/java/com/algorithm/test1769.java
@@ -0,0 +1,18 @@
+package com.algorithm;
+
+public class test1769 {
+    public int[] minOperations(String boxes) {
+        int n=boxes.length();
+        int[] res=new int[n];
+        for (int i=0;i<n;i++){
+            int sm=0;
+            for (int j=0;j<n;j++){
+                if (boxes.charAt(j)=='1'){
+                    sm+=Math.abs(j-i);
+                }
+            }
+            res[i]=sm;
+        }
+        return res;
+    }
+}
