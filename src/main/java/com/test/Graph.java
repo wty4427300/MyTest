5fc1b8c1e72eb620f9d33329794b0bba88f7481b
wty4427300 <wty4427300@gmail.com>
2022-12-19 10:46:53 +0800

    邻接表的初始化和两种遍历方式

--- src/main/java/com/test/Graph.java
+++ src/main/java/com/test/Graph.java
@@ -71,33 +71,32 @@ public class Graph {
 
     /**
      * @param s
-     * @param t
-     * 深度遍历
+     * @param t 深度遍历
      */
     public void dfs(int s, int t) {
         found = false;
         boolean[] visited = new boolean[v];
-        int[] prev=new int[v];
+        int[] prev = new int[v];
         //初始化遍历路径
-        Arrays.fill(prev,-1);
+        Arrays.fill(prev, -1);
         recurDfs(s, t, visited, prev);
         this.print(prev, s, t);
     }
 
-    private void recurDfs(int w,int t,boolean[] visited,int[] prev){
-        if (found){
+    private void recurDfs(int w, int t, boolean[] visited, int[] prev) {
+        if (found) {
             return;
         }
-        visited[w]=true;
-        if (w==t){
-            found=true;
+        visited[w] = true;
+        if (w == t) {
+            found = true;
             return;
         }
-        for (int i=0;i<adj[w].size();i++){
-            int next=adj[w].get(i);
-            if (!visited[next]){
-                prev[next]=w;
-                recurDfs(next,t,visited,prev);
+        for (int i = 0; i < adj[w].size(); i++) {
+            int next = adj[w].get(i);
+            if (!visited[next]) {
+                prev[next] = w;
+                recurDfs(next, t, visited, prev);
             }
         }
     }
