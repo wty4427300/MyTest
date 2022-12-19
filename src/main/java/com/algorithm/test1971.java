8f8bddab31f2434b205df066982af033b31959a2
wty4427300 <wty4427300@gmail.com>
2022-12-19 11:06:20 +0800

    一题两解

--- src/main/java/com/algorithm/test1971.java
+++ src/main/java/com/algorithm/test1971.java
@@ -6,34 +6,69 @@ import java.util.List;
 import java.util.Queue;
 
 public class test1971 {
+    /**
+     * 层级遍历
+     */
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         //初始化图
-        List<Integer>[] adj=new List[n];
-        for (int i=0;i<n;i++){
-            adj[i]= new ArrayList<>();
+        List<Integer>[] adj = new List[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
         }
         //遍历所有的边,初始化邻接表(双向图)
-        for (int[] edge:edges){
-            int x=edge[0],y=edge[1];
+        for (int[] edge : edges) {
+            int x = edge[0], y = edge[1];
             adj[x].add(y);
             adj[y].add(x);
         }
-        boolean[] visited=new boolean[n];
-        Queue<Integer> queue=new ArrayDeque<>();
+        boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new ArrayDeque<>();
         queue.offer(source);
-        visited[source]=true;
-        while (!queue.isEmpty()){
-            int vertex=queue.poll();
-            if (vertex==destination){
+        visited[source] = true;
+        while (!queue.isEmpty()) {
+            int vertex = queue.poll();
+            if (vertex == destination) {
                 break;
             }
-            for (int next:adj[vertex]){
-                if (!visited[next]){
+            for (int next : adj[vertex]) {
+                if (!visited[next]) {
                     queue.offer(next);
-                    visited[next]=true;
+                    visited[next] = true;
                 }
             }
         }
         return visited[destination];
+    }
+
+    /**
+     * 深度遍历
+     */
+    public boolean validPath1(int n, int[][] edges, int source, int destination) {
+        //初始化图
+        List<Integer>[] adj = new List[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        //遍历所有的边,初始化邻接表(双向图)
+        for (int[] edge : edges) {
+            int x = edge[0], y = edge[1];
+            adj[x].add(y);
+            adj[y].add(x);
+        }
+        boolean[] visited = new boolean[n];
+        return this.dfs(source, destination, adj, visited);
+    }
+
+    private boolean dfs(int source, int destination, List<Integer>[] adj, boolean[] visited) {
+        if (source == destination) {
+            return true;
+        }
+        visited[source] = true;
+        for (int next : adj[source]) {
+            if (!visited[next] && dfs(next, destination, adj, visited)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
