95c58ddf71e98edd46e972943173ea12e13df0e4
wty4427300 <wty4427300@gmail.com>
2022-12-20 10:48:47 +0800

    二分法求值

---
+++ src/main/java/com/algorithm/test1760.java
@@ -0,0 +1,24 @@
+package com.algorithm;
+
+import java.util.Arrays;
+
+public class test1760 {
+    public int minimumSize(int[] nums, int maxOperations) {
+        int left = 1, right = Arrays.stream(nums).max().getAsInt();
+        int ans = 0;
+        while (left <= right) {
+            int y = (left + right) / 2;
+            long ops = 0;
+            for (int x : nums) {
+                ops += (x - 1) / y;
+            }
+            if (ops <= maxOperations) {
+                ans = y;
+                right = y - 1;
+            } else {
+                left = y + 1;
+            }
+        }
+        return ans;
+    }
+}
