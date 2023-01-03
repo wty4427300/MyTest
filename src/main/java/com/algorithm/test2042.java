daad239a844924ce97a838bb34b1d483ecaecfe4
wty4427300 <wty4427300@gmail.com>
2023-01-03 09:47:59 +0800

    简单题,简单做

---
+++ src/main/java/com/algorithm/test2042.java
@@ -0,0 +1,23 @@
+package com.algorithm;
+
+public class test2042 {
+    public boolean areNumbersAscending(String s) {
+        int pre = 0, pos = 0;
+        while (pos < s.length()) {
+            if (Character.isDigit(s.charAt(pos))) {
+                int cur = 0;
+                while (pos < s.length() && Character.isDigit(s.charAt(pos))) {
+                    cur = cur * 10 + s.charAt(pos) - '0';
+                    pos++;
+                }
+                if (cur <= pre) {
+                    return false;
+                }
+                pre = cur;
+            } else {
+                pos++;
+            }
+        }
+        return true;
+    }
+}
