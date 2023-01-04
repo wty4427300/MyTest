f8d90e10b62ad6d7e3bd0e3e6100445bd7896742
wty4427300 <wty4427300@gmail.com>
2023-01-04 10:13:08 +0800

    贪心加二分

---
+++ src/main/java/com/algorithm/test1802.java
@@ -0,0 +1,32 @@
+package com.algorithm;
+
+public class test1802 {
+    public int maxValue(int n, int index, int maxSum) {
+        int left = 1, right = maxSum;
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            if (this.valid(mid, n, index, maxSum)) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    public boolean valid(int mid, int n, int index, int maxSum) {
+        int left = index;
+        int right = n - index - 1;
+        return mid + this.cal(mid, left) + this.cal(mid, right) <= maxSum;
+    }
+
+    public long cal(int big, int length) {
+        if (length + 1 < big) {
+            int small = big - length;
+            return (long) (big - 1 + small) * length / 2;
+        } else {
+            int ones = length - (big - 1);
+            return (long) big * (big - 1) / 2 + ones;
+        }
+    }
+}
---
+++ src/main/java/com/ftest/singleton/Singleton.java
@@ -0,0 +1,25 @@
+package com.ftest.singleton;
+
+/**
+ * 双检锁单例
+ */
+public class Singleton {
+
+    private volatile static Singleton instance;
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Singleton.getInstance();
+    }
+
+}
