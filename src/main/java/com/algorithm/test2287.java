f9f976a6369b1f96dccd5911e42c9bd7662aeb21
wty4427300 <wty4427300@gmail.com>
2023-01-13 10:46:33 +0800

    简化一下代码

--- src/main/java/com/algorithm/test2287.java
+++ src/main/java/com/algorithm/test2287.java
@@ -5,8 +5,8 @@ import java.util.Map;
 
 public class test2287 {
     public int rearrangeCharacters(String s, String target) {
-        Map<Character, Integer> sMap = new HashMap<Character, Integer>();
-        Map<Character, Integer> targetMap = new HashMap<Character, Integer>();
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> targetMap = new HashMap<>();
         int n = s.length(), m = target.length();
         //统计target次数
         for (int i = 0; i < m; i++) {
@@ -24,11 +24,11 @@ public class test2287 {
         for (Map.Entry<Character, Integer> entry : targetMap.entrySet()) {
             char c = entry.getKey();
             int count = entry.getValue();
-            int totalCount = sMap.getOrDefault(c, 0);
-            //组成最大副本数为数量最少的那个字符,即木桶的最短板
-            ans = Math.min(ans, totalCount / count);
-            //有一个字符不存在就无法组成
-            if (ans == 0) {
+            if (sMap.containsKey(c)) {
+                int totalCount = sMap.get(c);
+                //组成最大副本数为数量最少的那个字符,即木桶的最短板
+                ans = Math.min(ans, totalCount / count);
+            } else {
                 return 0;
             }
         }
