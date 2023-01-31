7128ea8bbe9afcb6d8be57fdbf49923343ddda6d
wty4427300 <wty4427300@gmail.com>
2023-01-31 10:02:00 +0800

    leetcode2319

--- src/main/java/com/algorithm/test2319.java
+++ src/main/java/com/algorithm/test2319.java
@@ -1,5 +1,18 @@
 package com.algorithm;
 
 public class test2319 {
-
+    public boolean checkXMatrix(int[][] grid) {
+        int n = grid.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                //切点
+                if (i == j || i + j + 1 == n) {
+                    if (grid[i][j] == 0) return false;
+                } else if (grid[i][j] != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
