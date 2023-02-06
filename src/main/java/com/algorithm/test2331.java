666f6a721d132faa706f225b0596c2b357a9f009
wty4427300 <wty4427300@gmail.com>
2023-02-06 09:54:02 +0800

    简单的递归

---
+++ src/main/java/com/algorithm/test2331.java
@@ -0,0 +1,16 @@
+package com.algorithm;
+
+public class test2331 {
+    public boolean evaluateTree(TreeNode root) {
+        if (root.left == null) {
+            return root.val == 1;
+        }
+        if (root.val == 2) {
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        } else {
+            return evaluateTree(root.left) && evaluateTree(root.right);
+        }
+    }
+
+
+}
