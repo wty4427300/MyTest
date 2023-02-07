8defc28d46a0fbb59a6fe478429290a756a76661
wty4427300 <wty4427300@gmail.com>
2023-02-07 18:05:04 +0800

    脑动大开写了一段

---
+++ src/main/java/com/test/LRUCache.java
@@ -0,0 +1,23 @@
+package com.test;
+
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache<K, V> extends LinkedHashMap<K, V> {
+    private int capacity;
+
+
+    public LRUCache(int capacity) {
+        super(capacity, 0.75f, true);
+        this.capacity = capacity;
+    }
+
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size() > capacity;
+    }
+}
+
+
