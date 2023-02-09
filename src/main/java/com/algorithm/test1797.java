c037e58f510e5de683a2146b933297e3e43bc6aa
wty4427300 <wty4427300@gmail.com>
2023-02-09 11:15:00 +0800

    map即可

---
+++ src/main/java/com/algorithm/test1797.java
@@ -0,0 +1,38 @@
+package com.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class test1797 {
+}
+
+class AuthenticationManager {
+
+    private int timeToLive;
+    private Map<String, Integer> map;
+
+    public AuthenticationManager(int timeToLive) {
+        this.timeToLive = timeToLive;
+        this.map = new HashMap<>();
+    }
+
+    public void generate(String tokenId, int currentTime) {
+        map.put(tokenId, currentTime + timeToLive);
+    }
+
+    public void renew(String tokenId, int currentTime) {
+        if (map.getOrDefault(tokenId, 0) > currentTime) {
+            map.put(tokenId, currentTime + timeToLive);
+        }
+    }
+
+    public int countUnexpiredTokens(int currentTime) {
+        int res = 0;
+        for (int time : map.values()) {
+            if (time > currentTime) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
