b963d8e20bcefc4ca2d8990b06c4e495e523fa23
wty4427300 <wty4427300@gmail.com>
2023-02-22 17:31:29 +0800

    表达式映射

---
+++ src/main/java/com/test/test7.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import java.lang.reflect.Method;
+
+
+class Bean {
+    int id=1;
+    public int getId() {
+        return id;
+    }
+}
+public class test7 {
+    public static void main(String[] args) throws Exception {
+        long start = System.currentTimeMillis();
+        Method methodGetId = Bean.class.getMethod("getId");
+        Bean bean = (Bean) Class.forName("com.test.Bean").newInstance();
+        int value = (Integer) methodGetId.invoke(bean);
+        System.out.println(value);
+        long end1 = System.currentTimeMillis();
+        System.out.println(end1-start);
+        java.util.function.ToIntFunction<Bean> function = Bean::getId;
+        int i = function.applyAsInt(bean);
+        System.out.println(i);
+        long end2 = System.currentTimeMillis();
+        System.out.println(end2-end1);
+    }
+}
