700e705f6800d431bc1c20fd9056362f21f61a58
wty4427300 <wty4427300@gmail.com>
2023-03-28 14:10:07 +0800

    动态规划

---
+++ src/main/java/com/algorithm/test1092.java
@@ -0,0 +1,38 @@
+package com.algorithm;
+
+public class test1092 {
+    public String shortestCommonSupersequence(String str1, String str2) {
+        int m = str1.length(), n = str2.length();
+        int[][] f = new int[m + 1][n + 1];
+        //最长公共子序列
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    f[i][j] = f[i - 1][j - 1] + 1;
+                } else {
+                    f[i][j] = Math.max(f[i - 1][j], f[i][j - 1]);
+                }
+            }
+        }
+        //最短公共子序列
+        int i = m, j = n;
+        StringBuilder ans = new StringBuilder();
+        while (i > 0 || j > 0) {
+            if (i == 0) {
+                ans.append(str2.charAt(--j));
+            } else if (j == 0) {
+                ans.append(str1.charAt(--i));
+            } else {
+                if (f[i][j] == f[i - 1][j]) {
+                    ans.append(str1.charAt(--i));
+                } else if (f[i][j] == f[i][j - 1]) {
+                    ans.append(str2.charAt(--j));
+                } else {
+                    ans.append(str1.charAt(--i));
+                    j--;
+                }
+            }
+        }
+        return ans.reverse().toString();
+    }
+}
