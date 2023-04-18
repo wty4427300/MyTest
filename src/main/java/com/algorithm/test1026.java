77231610e230a4b845dda962f828c10e0eb50fcc
wty4427300 <wty4427300@gmail.com>
2023-04-18 09:55:31 +0800

    递归

---
+++ src/main/java/com/algorithm/test1026.java
@@ -0,0 +1,20 @@
+package com.algorithm;
+
+public class test1026 {
+    public int maxAncestorDiff(TreeNode root) {
+        return dfs(root, root.val, root.val);
+    }
+
+    public int dfs(TreeNode root, int mi, int ma) {
+        if (root == null) {
+            return 0;
+        }
+        //计算当前节点和子孙节点绝对值的最大值
+        int diff = Math.max(Math.abs(root.val - mi), Math.abs(root.val - ma));
+        mi = Math.min(mi, root.val);
+        ma = Math.max(ma, root.val);
+        diff = Math.max(diff, dfs(root.left, mi, ma));
+        diff = Math.max(diff, dfs(root.right, mi, ma));
+        return diff;
+    }
+}
