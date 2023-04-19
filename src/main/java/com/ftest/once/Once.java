6ee6943aeac1628f417d4e2c01091a3cbeab2bdf
wty4427300 <wty4427300@gmail.com>
2023-04-19 14:55:25 +0800

    优化一下代码

--- src/main/java/com/ftest/once/Once.java
+++ src/main/java/com/ftest/once/Once.java
@@ -14,13 +14,9 @@ public class Once {
     public static void main(String[] args) {
         Once once = new Once();
         // 这个方法只会被执行一次
-        once.doOnce(() -> {
-            System.out.println("Hello, World!");
-        });
+        once.doOnce(() -> System.out.println("Hello, World!"));
         // 这个方法不会被执行
-        once.doOnce(() -> {
-            System.out.println("Hello, Java!");
-        });
+        once.doOnce(() -> System.out.println("Hello, Java!"));
 
     }
 }
