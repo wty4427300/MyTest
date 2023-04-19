f84e43a5461d8c6ec8e9948bf4aa11f3bc9c44ad
wty4427300 <wty4427300@gmail.com>
2023-04-19 12:21:02 +0800

    用java实现go的sync.once

---
+++ src/main/java/com/ftest/once/Once.java
@@ -0,0 +1,26 @@
+package com.ftest.once;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Once {
+    private AtomicBoolean executed = new AtomicBoolean(false);
+
+    public void doOnce(Runnable action) {
+        if (executed.compareAndSet(false, true)) {
+            action.run();
+        }
+    }
+
+    public static void main(String[] args) {
+        Once once = new Once();
+        // 这个方法只会被执行一次
+        once.doOnce(() -> {
+            System.out.println("Hello, World!");
+        });
+        // 这个方法不会被执行
+        once.doOnce(() -> {
+            System.out.println("Hello, Java!");
+        });
+
+    }
+}
