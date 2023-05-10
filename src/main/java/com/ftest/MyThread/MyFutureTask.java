98b8062a17651c73a17fc55af1ff8f0455d56eb7
wty4427300 <wty4427300@gmail.com>
2023-05-10 09:57:48 +0800

    优化future代码

--- src/main/java/com/ftest/MyThread/MyFutureTask.java
+++ src/main/java/com/ftest/MyThread/MyFutureTask.java
@@ -1,30 +1,35 @@
 package com.ftest.MyThread;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class MyFutureTask<T> implements Runnable{
-    Callable<T> callable;
-
-    T result;
-
-    volatile String state="NEW";
-
-    LinkedBlockingQueue<Thread> queue=new LinkedBlockingQueue<>();
+public class MyFutureTask<T> implements Runnable {
+    private Callable<T> callable;
+    private T result;
+    private volatile State state = State.NEW;
+    private BlockingQueue<Thread> queue = new ArrayBlockingQueue<>(1);
+    private final Lock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
 
     public MyFutureTask(Callable<T> callable) {
         this.callable = callable;
     }
 
-    public T get(){
-        //阻塞
-        if (state.equals("END")){
-            return result;
-        }
-        while (!"END".equals(state)){
-            queue.add(Thread.currentThread());
-            LockSupport.park();
+    public T get() throws InterruptedException {
+        lock.lock();
+        try {
+            if (state == State.NEW) {
+                queue.put(Thread.currentThread());
+                while (state != State.DONE) {
+                    condition.await();
+                }
+            }
+        } finally {
+            lock.unlock();
         }
         return result;
     }
@@ -32,16 +37,25 @@ public class MyFutureTask<T> implements Runnable{
     @Override
     public void run() {
         try {
-            result=callable.call();
+            result = callable.call();
+            state = State.DONE;
         } catch (Exception e) {
             e.printStackTrace();
-        }finally {
-            state="END";
+        } finally {
+            Thread thread = queue.poll();
+            while (thread != null) {
+                lock.lock();
+                try {
+                    condition.signalAll();
+                } finally {
+                    lock.unlock();
+                }
+                thread = queue.poll();
+            }
         }
-        Thread thread=queue.poll();
-        while (queue!=null){
-            LockSupport.unpark(thread);
-            thread=queue.poll();
-        }
+    }
+
+    private enum State {
+        NEW, DONE
     }
 }
