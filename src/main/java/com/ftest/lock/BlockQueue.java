bc12833ea152bba917a93f3c515ef74d075acac9
wty4427300 <wty4427300@gmail.com>
2023-06-06 17:11:57 +0800

    修改一下之前的代码

--- src/main/java/com/ftest/lock/BlockQueue.java
+++ src/main/java/com/ftest/lock/BlockQueue.java
@@ -1,46 +1,85 @@
 package com.ftest.lock;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class BlockQueue<T> {
-    private volatile boolean state;
-    final Lock lock=new ReentrantLock();
+    private final Integer cap;
+    private final Queue<T> queue;
+    private final Lock lock = new ReentrantLock();
     //队列不满
-    final Condition notFull=lock.newCondition();
+    private final Condition notFull = lock.newCondition();
     //队列不为空
-    final Condition netEmpty=lock.newCondition();
-    void enq(T x) throws InterruptedException {
+    private final Condition netEmpty = lock.newCondition();
+
+    public BlockQueue(Integer cap) {
+        this.cap = cap;
+        this.queue = new ArrayDeque<>(cap);
+    }
+
+    public void enq(T x) throws InterruptedException {
         lock.lock();
         try {
             //条件为队列已满
-            while (state){
+            while (queue.size() == cap) {
+                System.out.println("队列已满");
                 notFull.await();
             }
             //入队列操作
-
-            System.out.println("shabi");
-            //如队列之后通知可以出队列
+            queue.add(x);
+            //入队列之后通知可以出队列
             netEmpty.signal();
-        }finally {
+        } finally {
             lock.unlock();
         }
     }
-    void deq(T x) throws InterruptedException {
+
+    public T deq() throws InterruptedException {
         lock.lock();
+        T t;
         try {
             //队列以空
-            while (state){
-                //等待队列不为空的时候
+            while (queue.isEmpty()) {
+                System.out.println("队列以空");
                 netEmpty.await();
             }
             //省略出队列操作
-            System.out.println("shabi");
+            t = queue.poll();
             //出队列后，通知可以入队列
             notFull.signal();
-        }finally {
+        } finally {
             lock.unlock();
         }
+        return t;
+    }
+
+    public static void main(String[] args) {
+        BlockQueue<Integer> queue = new BlockQueue<>(5);
+        Thread thread1= new Thread(()->{
+            for (int i = 0; i < 100; i++) {
+                try {
+                    queue.enq(i);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        Thread thread2= new Thread(()->{
+            for (int i = 0; i < 100; i++) {
+                try {
+                    Integer q = queue.deq();
+                    System.out.println(q);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
     }
 }
