4ff4d8e00125c13abe93220d52963b8d5ad990dd
wty <wty4427300@gmail.com>
2023-06-21 22:24:00 +0800

    解决注入失败的问题

--- src/main/java/com/pipeline/v2/test/service/IChargeModelHandler.java
+++ src/main/java/com/pipeline/v2/test/service/IChargeModelHandler.java
@@ -2,7 +2,10 @@ package com.pipeline.v2.test.service;
 
 import com.pipeline.v2.test.pojo.ChargeModel;
 import org.springframework.plugin.core.Plugin;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+@Component
 public interface IChargeModelHandler extends Plugin<ChargeModel> {
 
     void handleChargeModel(ChargeModel chargeModel);
