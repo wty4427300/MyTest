63831a0a269b83cb2b81f72d81f97507bd2f4099
wty4427300 <wty4427300@gmail.com>
2023-07-13 11:01:54 +0800

    顺序打印abc,降低了jdk的版本所以虚拟线程不能用了

---
+++ src/main/java/com/concurrent/condition/PrintABC.java
@@ -0,0 +1,94 @@
+package com.concurrent.condition;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PrintABC {
+    private final Lock lock = new ReentrantLock();
+    private final Condition conditionA = lock.newCondition();
+    private final Condition conditionB = lock.newCondition();
+    private final Condition conditionC = lock.newCondition();
+    private volatile int flag = 1;
+
+    public void printA() throws InterruptedException {
+        lock.lock();
+        try {
+            while (flag != 1) {
+                conditionA.await();
+            }
+            System.out.print("A");
+            flag = 2;
+            conditionB.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void printB() throws InterruptedException {
+        lock.lock();
+        try {
+            while (flag != 2) {
+                conditionB.await();
+            }
+            System.out.print("B");
+            flag = 3;
+            conditionC.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void printC() throws InterruptedException {
+        lock.lock();
+        try {
+            while (flag != 3) {
+                conditionC.await();
+            }
+            System.out.print("C");
+            System.out.println(); // 换行
+            flag = 1;
+            conditionA.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintABC printABC = new PrintABC();
+
+        Thread threadA = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    printABC.printA();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread threadB = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    printABC.printB();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread threadC = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    printABC.printC();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+    }
+}
--- src/main/java/com/virtual_threads/VirtualThreadConfig.java
+++ src/main/java/com/virtual_threads/VirtualThreadConfig.java
@@ -1,30 +1,28 @@
-package com.virtual_threads;
-
-import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.core.task.support.TaskExecutorAdapter;
-
-import java.util.concurrent.Executors;
-
-@Configuration
-public class VirtualThreadConfig {
-
-    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
-    public AsyncTaskExecutor asyncTaskExecutor() {
-        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
-    }
-
-    /**
-     * 这个 Bean 是一个自定义的 Tomcat 协议处理器，负责处理 Spring Boot 应用程序中传入的请求。
-     * 这个自定义程序的目的是配置协议处理器使用的执行器。
-     */
-    @Bean
-    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
-        return protocolHandler -> {
-            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
-        };
-    }
-}
+//package com.virtual_threads;
+//
+//import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+//import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.task.AsyncTaskExecutor;
+//import org.springframework.core.task.support.TaskExecutorAdapter;
+//
+//import java.util.concurrent.Executors;
+//
+//@Configuration
+//public class VirtualThreadConfig {
+//
+//    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+//    public AsyncTaskExecutor asyncTaskExecutor() {
+//        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+//    }
+//
+//    /**
+//     * 这个 Bean 是一个自定义的 Tomcat 协议处理器，负责处理 Spring Boot 应用程序中传入的请求。
+//     * 这个自定义程序的目的是配置协议处理器使用的执行器。
+//     */
+//    @Bean
+//    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+//        return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+//    }
+//}
