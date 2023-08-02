92349bea555513619ab81dcd90cb5375a7a16641
wty <wty4427300@gmail.com>
2023-08-02 12:01:57 +0800

    面试题

---
+++ src/main/java/com/test/test12.java
@@ -0,0 +1,45 @@
+package com.test;
+
+import com.algorithm.ArrayQueue;
+import lombok.Data;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Data
+class User {
+    private Long id;
+    private String name;
+}
+
+public class test12 {
+    public boolean test(String s) {
+        Queue<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+                stack.add(s.charAt(i));
+            } else {
+                char c = stack.peek();
+                if (c == '(' && s.charAt(i) == ')') {
+                    stack.poll();
+                } else if (c == '[' && s.charAt(i) == ']') {
+                    stack.poll();
+                } else if (c == '{' && s.charAt(i) == '}') {
+                    stack.poll();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        test12 test12 = new test12();
+        String s = "()[]{}";
+        boolean t1 = test12.test(s);
+        System.out.println(t1);
+        List<User> userList = new ArrayList<>();
+        userList.stream().collect(Collectors.toMap(User::getId, user -> user));
+    }
+}
