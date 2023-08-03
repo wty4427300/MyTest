2318715a85ba3ea29735e9b5f7e2f83b690978a1
wty4427300 <wty4427300@gmail.com>
2023-08-03 18:25:01 +0800

    今日leetcode

--- src/main/java/com/algorithm/test722.java
+++ src/main/java/com/algorithm/test722.java
@@ -1,43 +1,47 @@
-//package com.algorithm;
-// todo
-//import java.util.ArrayDeque;
-//import java.util.Arrays;
-//import java.util.List;
-//import java.util.Queue;
-//
-//public class test722 {
-//    public List<String> removeComments(String[] source) {
-//        Queue<String> queue=new ArrayDeque<>();
-//        Arrays.stream(source).filter(
-//                it->{
-//                    queue.
-//                    for(int i=0;i<it.length();i++){
-//                        if (it.charAt(i)=='/'){
-//                            if (it.charAt(i+1)=='/'){
-//                                //单行注释
-//                                return false;
-//                            }else {
-//                                //可能多行注释
-//                                queue.add()
-//                            }
-//                        }
-//                    }
-//                }
-//        )
-//    }
-//
-//    public static void main(String[] args) {
-//        String[] source = new String[]{
-//                "/*Test program */",
-//                "int main()",
-//                "{ ",
-//                "  // variable declaration ",
-//                "int a, b, c;",
-//                "/* This is a test",
-//                "   multiline  ",
-//                "   comment for ",
-//                "   testing */",
-//                "a = b + c;",
-//                "}"};
-//    }
-//}
+package com.algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class test722 {
+    private final int exist = 0;
+
+    public List<String> removeComments(String[] source) {
+        List<String> res=new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        boolean isBlank=false;
+        for (String s:source){
+            for (int i = 0; i < s.length(); i++) {
+                if (isBlank){
+                    if (i + 1<s.length() && s.charAt(i) == '*' && s.charAt(i + 1) == '/') {
+                        isBlank = false;
+                        i++;
+                    }
+                }else {
+                    if (i + 1<s.length() && s.charAt(i) == '/' && s.charAt(i + 1) == '*') {
+                        isBlank = true;
+                        i++;
+                    } else if (i + 1<s.length() &&s.charAt(i) == '/' && s.charAt(i + 1) == '/') {
+                        break;
+                    } else {
+                        builder.append(s.charAt(i));
+                    }
+                }
+            }
+            if (!isBlank && !builder.isEmpty()){
+                res.add(builder.toString());
+                builder.setLength(0);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String[] source = new String[]{"void func(int k) {", "// this function does nothing /*", "   k = k*2/4;", "   k = k/2;*/", "}"};
+        test722 test722 = new test722();
+        List<String> strings = test722.removeComments(source);
+        System.out.println(strings);
+    }
+}
