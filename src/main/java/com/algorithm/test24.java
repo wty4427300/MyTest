081319bfa08f44c00a02e2a076b20f9941340458
wty <wty4427300@gmail.com>
2023-08-06 20:59:15 +0800

    游戏打累了刷道题,递归

---
+++ src/main/java/com/algorithm/test24.java
@@ -0,0 +1,13 @@
+package com.algorithm;
+
+public class test24 {
+    public ListNode swapPairs(ListNode head) {
+        if (head==null || head.next==null){
+            return head;
+        }
+        ListNode newHead=head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next=head;
+        return newHead;
+    }
+}
