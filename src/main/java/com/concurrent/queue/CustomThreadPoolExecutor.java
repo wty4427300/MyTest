d40dbdcc18df8375b6310eaa317bfd920939339f
wty4427300 <wty4427300@gmail.com>
2023-08-07 09:42:52 +0800

    一次失败的尝试

---
+++ src/main/java/com/concurrent/queue/CustomThreadPoolExecutor.java
@@ -0,0 +1,75 @@
+package com.concurrent.queue;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+@Getter
+class MyTask implements Runnable {
+    private final String id;
+    private final Runnable task;
+
+    public MyTask(String id, Runnable task) {
+        this.id = id;
+        this.task = task;
+    }
+
+    @Override
+    public void run() {
+        task.run();
+    }
+}
+
+public class CustomThreadPoolExecutor extends ThreadPoolExecutor {
+    private final ConcurrentHashMap<Integer, Thread> threadMap = new ConcurrentHashMap<>();
+
+    public CustomThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit,
+                                    BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        if (command instanceof MyTask) {
+            int id = ((MyTask) command).getId().hashCode() % 10;
+            threadMap.computeIfAbsent(id, key -> {
+                Thread thread = getThreadFactory().newThread(command);
+                thread.start();
+                return thread;
+            });
+        }
+    }
+
+    @Override
+    public void shutdown(){
+        super.shutdown();
+    }
+
+    public static void main(String[] args) {
+        CustomThreadPoolExecutor pool = new CustomThreadPoolExecutor(
+                10,
+                10,
+                1L,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(100),
+                new ThreadPoolExecutor.CallerRunsPolicy()
+        );
+
+        List<Message> messages = new ArrayList<>();
+        messages.add(new Message(0, "order init"));
+        messages.add(new Message(0, "order pay"));
+        messages.add(new Message(1, "order init"));
+        messages.add(new Message(1, "order pay"));
+        messages.add(new Message(2, "order init"));
+        messages.add(new Message(2, "order pay"));
+
+        for (Message msg : messages) {
+            pool.execute(new MyTask(msg.getId().toString(), () -> System.out.println(msg.getMessage())));
+        }
+
+        pool.shutdown();
+    }
+}
+
