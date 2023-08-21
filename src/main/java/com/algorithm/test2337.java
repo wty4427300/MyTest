998f238447b8ebb40a0f9c30a3da5b710ada8be8
wty4427300 <wty4427300@gmail.com>
2023-08-21 10:25:25 +0800

    很巧妙的一道题目

---
+++ src/main/java/com/algorithm/test2337.java
@@ -0,0 +1,22 @@
+package com.algorithm;
+
+public class test2337 {
+    public boolean canChange(String start, String target) {
+        if (!start.replaceAll("_", "").equals(target.replaceAll("_", ""))) {
+            return false;
+        }
+        for (int i = 0, j = 0; i < start.length(); i++) {
+            if (start.charAt(i) == '_') {
+                continue;
+            }
+            while (target.charAt(j) == '_') {
+                j++;
+            }
+            if (i != j && (start.charAt(i) == 'L') == (i < j)) {
+                return false;
+            }
+            j++;
+        }
+        return true;
+    }
+}
