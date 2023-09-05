e40dc9eca0e252fa62ab42aea1c0e9fa8c487ad8
wty4427300 <wty4427300@gmail.com>
2023-09-05 10:44:34 +0800

    简单题

--- src/main/java/com/kafka/KafkaConsumerAnalysis.java
+++ src/main/java/com/kafka/KafkaConsumerAnalysis.java
@@ -6,7 +6,6 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import java.time.Duration;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;
