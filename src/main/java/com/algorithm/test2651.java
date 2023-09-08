326b49e188beb196b370af32e2c7c26a1f5985b9
wty4427300 <wty4427300@gmail.com>
2023-09-08 10:48:12 +0800

    简单题目

--- src/main/java/com/algorithm/test2651.java
+++ src/main/java/com/algorithm/test2651.java
@@ -3,9 +3,7 @@ package com.algorithm;
 public class test2651 {
     public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
         int realTime = arrivalTime + delayedTime;
-        if (realTime==24){
-            return 0;
-        }else if (realTime>24){
+        if (realTime>=24){
             return realTime-24;
         }
         return realTime;
