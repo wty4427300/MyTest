7e7c6297afb7768dc79440c269c00f16057c60a1
wty4427300 <wty4427300@gmail.com>
2023-09-26 10:00:23 +0800

    简单题

---
+++ src/main/java/com/algorithm/test2582.java
@@ -0,0 +1,9 @@
+package com.algorithm;
+
+public class test2582 {
+    public int passThePillow(int n, int time) {
+        int k = time / (n - 1);
+        int mod = time % (n - 1);
+        return (k & 1) == 1 ? n - mod : mod + 1;
+    }
+}
