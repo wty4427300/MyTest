ea5a5a76cf3322f90111b1fdfb2f85c228fd41b4
wty4427300 <wty4427300@gmail.com>
2023-10-19 17:01:41 +0800

    hash+计数

---
+++ src/main/java/com/algorithm/test1726.java
@@ -0,0 +1,22 @@
+package com.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class test1726 {
+    public int tupleSameProduct(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int key = nums[i] * nums[j];
+                cnt.put(key, cnt.getOrDefault(key, 0) + 1);
+            }
+        }
+        int ans = 0;
+        for (Integer v : cnt.values()) {
+            ans += v * (v - 1) * 4;
+        }
+        return ans;
+    }
+}
