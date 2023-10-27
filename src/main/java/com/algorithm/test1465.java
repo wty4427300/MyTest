535ef6fe88f5c284da5dde807ac4da3812d79799
wty4427300 <wty4427300@gmail.com>
2023-10-27 17:56:08 +0800

    贪心

---
+++ src/main/java/com/algorithm/test1465.java
@@ -0,0 +1,19 @@
+package com.algorithm;
+
+import java.util.Arrays;
+
+public class test1465 {
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+        return (int) ((long) calMax(horizontalCuts, h) * calMax(verticalCuts, w) % 1000000007);
+    }
+    public int calMax(int[] arr, int boardr) {
+        int res = 0, pre = 0;
+        for (int i : arr) {
+            res = Math.max(i - pre, res);
+            pre = i;
+        }
+        return Math.max(res, boardr - pre);
+    }
+}
---
+++ src/main/java/com/algorithm/test2520.java
@@ -0,0 +1,7 @@
+package com.algorithm;
+
+public class test2520 {
+    public int countDigits(int num) {
+        return 0;
+    }
+}
