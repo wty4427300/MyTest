1d50c8ecb95809cc60b26642de61cb54e0d9f804
wty4427300 <wty4427300@gmail.com>
2023-10-31 10:55:53 +0800

    给leetcode275补充一个测试用例

--- src/main/java/com/algorithm/test275.java
+++ src/main/java/com/algorithm/test275.java
@@ -2,7 +2,9 @@ package com.algorithm;
 
 public class test275 {
     public int hIndex(int[] citations) {
+        //n是论文总篇数
         int n = citations.length;
+        //因为一定存在0篇论文引用0次,所以最小引用数从1开始
         int left = 1, right = n;
         while (left <= right) {
             int mid = (left + right) >>> 1;
@@ -13,5 +15,12 @@ public class test275 {
             }
         }
         return right;
+    }
+
+    public static void main(String[] args) {
+        int[] citations = new int[]{0, 1, 3, 5, 6};
+        test275 test275 = new test275();
+        int ans = test275.hIndex(citations);
+        System.out.println(ans);
     }
 }
