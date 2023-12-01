3d0f716a3203e12a246f098f710b4a0693f5d107
wty4427300 <wty4427300@gmail.com>
2023-12-01 11:04:11 +0800

    字符串相似

---
+++ src/main/java/com/algorithm/test1657.java
@@ -0,0 +1,23 @@
+package com.algorithm;
+
+import java.util.Arrays;
+
+public class test1657 {
+    public boolean closeStrings(String word1, String word2) {
+        int[] count1 = new int[26], count2 = new int[26];
+        for (char c : word1.toCharArray()) {
+            count1[c - 'a']++;
+        }
+        for (char c : word2.toCharArray()) {
+            count2[c - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count1[i] > 0 && count2[i] == 0 || count1[i] == 0 && count2[i] > 0) {
+                return false;
+            }
+        }
+        Arrays.sort(count1);
+        Arrays.sort(count2);
+        return Arrays.equals(count1, count2);
+    }
+}
