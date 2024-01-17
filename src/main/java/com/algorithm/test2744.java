d194e696ae43a4b413df2566b264efd85f723b69
wty4427300 <wty4427300@gmail.com>
2024-01-17 14:28:07 +0800

    遍历枚举

---
+++ src/main/java/com/algorithm/test2744.java
@@ -0,0 +1,16 @@
+package com.algorithm;
+
+public class test2744 {
+    public int maximumNumberOfStringPairs(String[] words) {
+        int n= words.length;
+        int ans=0;
+        for (int i=0;i<n;i++){
+            for (int j=i+1;j<n;j++){
+                if (words[i].charAt(0)==words[j].charAt(1) && words[i].charAt(1) == words[j].charAt(0)){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
