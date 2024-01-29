2dfe9a8e94635335e7a378e9bdbd58051d9c3887
wty4427300 <wty4427300@gmail.com>
2024-01-29 16:39:13 +0800

    复习kafka

--- src/main/java/com/kafka/KafkaConsumerAnalysis.java
+++ src/main/java/com/kafka/KafkaConsumerAnalysis.java
@@ -1,11 +1,10 @@
 package com.kafka;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.errors.WakeupException;
 
 import java.time.Duration;
 import java.util.*;
@@ -79,9 +78,11 @@ public class KafkaConsumerAnalysis {
             set.add(tp);
             Map<TopicPartition, OffsetAndMetadata> committed = consumer.committed(set);
             OffsetAndMetadata offsetAndMetadata = committed.get(tp);
-            System.out.println("committed offset is " + offsetAndMetadata.offset());
-            long position = consumer.position(tp);
-            System.out.println("the offset of the next record is " + position);
+            if (ObjectUtils.isNotEmpty(offsetAndMetadata)) {
+                System.out.println("committed offset is " + offsetAndMetadata.offset());
+                long position = consumer.position(tp);
+                System.out.println("the offset of the next record is " + position);
+            }
         }
     }
 
@@ -130,6 +131,8 @@ public class KafkaConsumerAnalysis {
 
     /**
      * 单次提交
+     * 需要提交的是下一条要消费的消息的 offset，即 lastConsumedOffset + 1。
+     * 这样做可以确保下一次消费者启动时从正确的位置开始消费消息。
      */
     public void test5() {
         Properties props = initConfig();
@@ -152,7 +155,7 @@ public class KafkaConsumerAnalysis {
     /**
      * partition粒度的单次提交
      */
-    public void test6(){
+    public void test6() {
         Properties props = initConfig();
         try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
             consumer.subscribe(List.of(topic));
@@ -162,13 +165,94 @@ public class KafkaConsumerAnalysis {
                     List<ConsumerRecord<String, String>> partitionRecords =
                             records.records(partition);
                     for (ConsumerRecord<String, String> record : partitionRecords) {
-                        //do some logical processing.
+                        System.out.println("topic = " + record.topic()
+                                + ", partition = " + record.partition()
+                                + ", offset = " + record.offset());
+                        System.out.println("key = " + record.key()
+                                + ", value = " + record.value());
                     }
                     long lastConsumedOffset = partitionRecords
                             .get(partitionRecords.size() - 1).offset();
                     consumer.commitSync(Collections.singletonMap(partition,
                             new OffsetAndMetadata(lastConsumedOffset + 1)));
                 }
+            }
+        }
+    }
+
+    /**
+     * 异步提交,多次异步提交其中一次发生异常,导致offset回滚导致重复消费
+     */
+    public void test7() {
+        Properties props = initConfig();
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            consumer.subscribe(List.of(topic));
+            while (isRunning.get()) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println("topic = " + record.topic()
+                            + ", partition = " + record.partition()
+                            + ", offset = " + record.offset());
+                    System.out.println("key = " + record.key()
+                            + ", value = " + record.value());
+                }
+                //异步提交
+                consumer.commitAsync((offsets, exception) -> {
+                    if (exception == null) {
+                        System.out.println(offsets);
+                    } else {
+                        log.error("fail to commit offsets {}", offsets, exception);
+                    }
+                });
+            }
+        }
+    }
+
+    public void test8() {
+        Properties props = initConfig();
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            consumer.subscribe(List.of(topic));
+            while (isRunning.get()) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println("topic = " + record.topic()
+                            + ", partition = " + record.partition()
+                            + ", offset = " + record.offset());
+                    System.out.println("key = " + record.key()
+                            + ", value = " + record.value());
+                }
+                //异步提交
+                consumer.commitAsync((offsets, exception) -> {
+                    if (exception == null) {
+                        System.out.println(offsets);
+                    } else {
+                        log.error("fail to commit offsets {}", offsets, exception);
+                    }
+                });
+            }
+        }
+    }
+
+    /**
+     * 一个相对完整的消费逻辑
+     */
+    public void test9() {
+        Properties props = initConfig();
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            consumer.subscribe(List.of(topic));
+            try {
+                while (isRunning.get()) {
+                    //consumer.poll(***)
+                    //process the record.
+                    //commit offset.
+                }
+            } catch (WakeupException e) {
+                // ingore the error
+            } catch (Exception e) {
+                // do some logic process.
+            } finally {
+                // maybe commit offset.
+                consumer.close();
             }
         }
     }
