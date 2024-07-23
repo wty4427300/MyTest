840f68df2196bb1f7d4c2cb4f2f47cffe282c06a
v-wangtianyi <v-wangtianyi@360.cn>
2024-07-23 09:48:28 +0800

    前缀和

---
+++ src/main/java/com/algorithm/test3096.java
@@ -0,0 +1,21 @@
+package com.algorithm;
+
+// todo
+public class test3096 {
+    public int minimumLevels(int[] possible) {
+        int n = possible.length;
+        int tot = 0;
+        for (int x : possible) {
+            tot += x == 1 ? 1 : -1;
+        }
+        int pre = 0;
+        for (int i = 0; i < n - 1; i++) {
+            pre += possible[i] == 1 ? 1 : -1;
+            if (2 * pre > tot) {
+                //关卡数等于索引+1
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+}
