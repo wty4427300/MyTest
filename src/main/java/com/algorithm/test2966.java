6f7edfe5e61b4908d24289896b38c4375d546ecf
v-wangtianyi <v-wangtianyi@360.cn>
2025-06-19 10:34:37 +0800

    贪心题

---
+++ src/main/java/com/algorithm/test2966.java
@@ -0,0 +1,20 @@
+package com.algorithm;
+
+import java.util.Arrays;
+
+public class test2966 {
+    public int[][] divideArray(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int[][] res = new int[n / 3][3];
+        for (int i = 0; i < n; i += 3) {
+            if (nums[i + 2] - nums[i] > k) {
+                return new int[0][];
+            }
+            res[i / 3][0] = nums[i];
+            res[i / 3][1] = nums[i + 1];
+            res[i / 3][2] = nums[i + 2];
+        }
+        return res;
+    }
+}
