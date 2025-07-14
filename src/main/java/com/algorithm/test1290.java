03f699fa1270b799357a9a81daac792587c3488c
v-wangtianyi <v-wangtianyi@360.cn>
2025-07-14 11:16:00 +0800

    简单的二进制计算

--- src/main/java/com/algorithm/test1290.java
+++ src/main/java/com/algorithm/test1290.java
@@ -4,6 +4,12 @@ import com.algorithm.base.ListNode;
 
 public class test1290 {
     public int getDecimalValue(ListNode head) {
-
+        ListNode curNode = head;
+        int ans = 0;
+        while (curNode != null) {
+            ans = ans * 2 + curNode.val;
+            curNode = curNode.next;
+        }
+        return ans;
     }
 }
